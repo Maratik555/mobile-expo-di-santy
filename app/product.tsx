35b7f0b89c94404f91db96e2892b3ba2fe248597
Marat <80047266+Maratik555@users.noreply.github.com>
2025-10-15 15:39:42 +0300

    Update product.tsx

--- app/product.tsx
+++ app/product.tsx
@@ -63,8 +63,9 @@ export default function ProductScreen() {
               <Text style={styles.colorLabel}>Цвет: </Text>
               <Text style={styles.colorValue}>{selectedColor}</Text>
               {/* <Text style={styles.chevron}>{isColorOpen ? '▲' : '▼'}</Text> */}
+              <Text style={styles.stock}>осталось 2шт на складе</Text>
             </View>
-            <Text style={styles.stock}>осталось 2шт на складе</Text>
+            
           </Pressable>
 
           {/* {isColorOpen && (
@@ -163,43 +164,45 @@ export default function ProductScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F7FA',
+    backgroundColor: '#FFF',
   },
   productImage: {
     width: '100%',
     height: '100%',
+    marginTop: 60,
   },
   imageContainer: {
     height: 350,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     position: 'relative',
     justifyContent: 'center',
     alignItems: 'center',
   },
   imagePlaceholder: {
-    width: '60%',
+    width: '70%',
     height: '70%',
-    backgroundColor: '#E0E7F0',
+    backgroundColor: '#FFF',
   },
   closeButton: {
     position: 'absolute',
-    top: 50,
-    right: 20,
+    top: 70,
+    right: 30,
     width: 30,
     height: 30,
     borderRadius: 15,
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
     alignItems: 'center',
     justifyContent: 'center',
   },
   closeButtonText: {
     fontSize: 18,
-    color: '#2C2C2C',
+    color: '#10366A',
+    fontWeight: 'bold',
   },
   contentContainer: {
-    backgroundColor: '#E8EEF7',
+    backgroundColor: '#E2EDFE',
     paddingHorizontal: 20,
     paddingTop: 20,
+    borderRadius: 15,
     paddingBottom: 140,
   },
   infoSection: {
@@ -207,7 +210,7 @@ const styles = StyleSheet.create({
   },
   category: {
     ...typography.heading3,
-    color: '#6B7C93',
+    color: '#6B83A4',
     marginBottom: 8,
   },
   row: {
@@ -216,17 +219,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   productName: {
-    ...typography.heading3,
-    color: '#2B4B7C',
+    ...typography.text2,
+    color: '#10366A',
   },
   price: {
     ...typography.price,
-    color: '#2C2C2C',
+    color: '#404040',
   },
   colorSection: {
     marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CBD5E1',
     paddingBottom: 15,
   },
   colorRow: {
@@ -237,45 +238,45 @@ const styles = StyleSheet.create({
   colorLabel: {
     fontSize: 14,
     fontWeight: '600',
-    color: '#2B4B7C',
+    color: '#10366A',
   },
   colorValue: {
     fontSize: 14,
-    color: '#6B7C93',
+    color: '#8C8C8C',
     flex: 1,
   },
   stock: {
     fontSize: 12,
-    color: '#8C9DB5',
+    color: '#8C8C8C',
   },
   colorOptions: {
     marginTop: 10,
     gap: 8,
   },
-  colorOption: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    borderWidth: 1,
-    borderColor: '#CBD5E1',
-  },
+  // colorOption: {
+  //   backgroundColor: '#FFFFFF',
+  //   borderRadius: 8,
+  //   paddingVertical: 12,
+  //   paddingHorizontal: 16,
+  //   borderWidth: 1,
+  //   borderColor: '#CBD5E1',
+  // },
   selectedColorOption: {
     backgroundColor: '#E8EEF7',
-    borderColor: '#2B4B7C',
+    borderColor: '#10366A',
   },
   colorOptionText: {
     fontSize: 14,
     color: '#4B5563',
   },
   selectedColorOptionText: {
-    color: '#2B4B7C',
+    color: '#10366A',
     fontWeight: '600',
   },
   collapsibleSection: {
     marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CBD5E1',
+    // borderBottomWidth: 1,
+    // borderBottomColor: '#CBD5E1',
     paddingBottom: 15,
   },
   collapsibleHeader: {
@@ -286,11 +287,11 @@ const styles = StyleSheet.create({
   },
   collapsibleTitle: {
     ...typography.heading3,
-    color: '#2B4B7C',
+    color: '#10366A',
   },
   chevron: {
     ...typography.text2,
-    color: '#2B4B7C',
+    color: '#10366A',
   },
   description: {
     ...typography.text2,
@@ -303,7 +304,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: '#E8EEF7',
+    backgroundColor: '#E2EDFE',
     paddingHorizontal: 20,
     paddingTop: 12,
     paddingBottom: 40,
@@ -315,12 +316,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   addedButton: {
-    backgroundColor: '#7B92B2',
+    backgroundColor: '#6B83A4',
   },
   addToCartText: {
     fontSize: 16,
     fontWeight: '600',
-    color: '#FFFFFF',
+    color: '#FFF',
   },
   toastContainer: {
     position: 'absolute',
@@ -331,12 +332,11 @@ const styles = StyleSheet.create({
   },
   toast: {
     flexDirection: 'row',
-    backgroundColor: '#E8EEF7',
-    borderRadius: 15,
-    padding: 15,
+    backgroundColor: '#E2EDFE',
+    borderRadius: 10,
     alignItems: 'center',
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.2,
     shadowRadius: 8,
     elevation: 5,
@@ -345,20 +345,20 @@ const styles = StyleSheet.create({
   toastText: {
     flex: 1,
     fontSize: 14,
-    color: '#2B4B7C',
+    color: '#10366A',
     fontWeight: '500',
     textAlign: 'center',
   },
   toastButton: {
     backgroundColor: '#F97C00',
-    borderRadius: 10,
     paddingHorizontal: 20,
-    paddingVertical: 10,
+    paddingVertical: 25,
     marginLeft: 10,
+    borderRadius: 10,
   },
   toastButtonText: {
     fontSize: 14,
     fontWeight: '600',
-    color: '#FFFFFF',
+    color: '#FFF',
   },
 });
